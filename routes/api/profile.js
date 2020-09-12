3b070362567423b473f9f73edc3cb28019c5834d
Aaryavrat Arora <arc252000@gmail.com>
2020-09-12 10:26:16 +0530

    another commit

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -161,4 +161,65 @@ router.delete("/", auth, async (req, res) => {
   }
 });
 
+// @route    PUT api/profile/experienct
+// @desc     Add profile experience
+// @access   Private
+router.put('/experience', [
+  auth,
+[
+   check('title', 'Title is required')
+    .not()
+     .isEmpty(),
+     check('company', 'Company is required')
+     .not()
+      .isEmpty(),
+      check('from', 'From date is required')
+    .not()
+     .isEmpty()
+]
+],
+async (req, res)=>{
+ const errors = validationResult(req);
+ 
+ if(!errors.isEmpty()){
+     return res.status(400).json({ errors: errors.array()});
+    }
+
+  const {
+    title,
+    company,
+    location,
+    from,
+    to,
+    current,
+    description
+  } = req.body;
+
+  const newExp = {
+    title,
+    company,
+    location,
+    from,
+    to,
+    current,
+    description
+  }
+
+  try {
+   const profile = await Profile.findOne({user: req.user.id});  
+   
+  //  unshift is same as push() but pusehes in start
+   profile.experience.unshift(newExp);
+
+   await profile.save();
+
+   res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.statur(500).send('Server Error');
+    
+  }
+})
+
+
 module.exports = router;
