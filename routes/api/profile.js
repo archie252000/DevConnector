53049bd4333ffd530a5ba69cafe5447c3187556d
Aaryavrat Arora <arc252000@gmail.com>
2020-09-19 02:20:33 +0530

    here comes another commit

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 const { check, validationResult } = require("express-validator");
-
+const request = require('request');
+const config = require('config');
 const Profile = require("../../models/Profile");
 const { profile_url } = require("gravatar");
 
@@ -320,5 +321,34 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
     res.status(500).send("Srever Error");
   }
 });
+
+// @route    GET api/profile/me
+// @desc     Get current users profile
+// @access   Private
+
+router.get('/github/:username', (req,res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5$sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: {'user-agent': 'node.js'}
+    };
+
+    request(options, (error, response, body) => {
+      if(error)
+       console.error(error);
+      
+       if(response.statusCode!==200){
+        return res.status(404).json({msg: 'No Github profile found'});
+      }
+
+      res.json(JSON.parse(body));
+
+    })
+  } catch (err) {
+    console.error(err.message);
+    res.status(500);
+  }
+})
 
 module.exports = router;
