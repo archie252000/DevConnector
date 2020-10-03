ff062da88c2c00e23bbcd6972bab7729c02e95b8
Aaryavrat Arora <arc252000@gmail.com>
2020-10-04 03:27:20 +0530

    lost track a long time ago, yet another commit

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,15 @@ import Landing from "./components/layouts/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+
 import "./App.css";
 
 const App = () => (
+  <Provider store = {store}>
   <Router>
   <Fragment>
     <Navbar />
@@ -20,6 +26,7 @@ const App = () => (
      </section>
   </Fragment>
   </Router> 
+</Provider>
 );
 
 export default App;
--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -21,6 +21,6 @@ const Navbar = () => {
         </li>
       </ul>
     </nav>
-  ); 
+  );
 };
 export default Navbar;
