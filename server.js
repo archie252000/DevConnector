2c2398c4bdb854a77cdefb6203601745819512b1
Aaryavrat Arora <arc252000@gmail.com>
2020-10-24 17:47:37 +0530

    RFD

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use("/api/posts", require("./routes/api/posts"));
 if(process.env.NODE_ENV === 'production'){
     //  Set the static folder
     app.use(express.static('client/build'));
-    app.get('*', (req,res) => {
+    app.get('*',(req,res) => {
      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));   
     });
 
